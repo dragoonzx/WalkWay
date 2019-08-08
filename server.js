875b41f1e2655f0f31fc7b49f8b6e07a81554803
Max <dragoonz@yandex.ru>
2019-08-08 09:24:04 +0300

    mmm

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express')
+var fs = require('fs')
+var http = require('http')
+var https = require('https')
+var redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
+
+var app = express()
+
+var privateKey = fs.readFileSync('keys/atelie72.key', 'utf8')
+var certificate = fs.readFileSync('keys/atelie72.crt', 'utf8')
+
+var port = 8080
+var sport = 4430
+
+var privateKey = fs.readFileSync('keys/atelie72.key', 'utf8')
+var certificate = fs.readFileSync('keys/atelie72.crt', 'utf8')
+
+var credentials = { key: privateKey, cert: certificate }
+
+var httpServer = http.createServer(app)
+var httpsServer = https.createServer(credentials, app)
+
+app.use(express.static(__dirname + '/build'))
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/build/index.html')
+})
+
+httpServer.listen(port, () => console.log('server is listening'))
+httpsServer.listen(sport, () => console.log('server is listening safely'))
